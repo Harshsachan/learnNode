7d77ab0950ac4932d405bb6d73d6736a2ab2d214
Harshsachan <harshit.26sachan@gmail.com>
2023-04-01 00:06:09 +0530

    test Commit

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+// npm used
+
 const _ = require('lodash');
 
 const items =[ 1,[2,3,[4,8,[5,[6,[7]]]]]]
 const newItems =_.flattenDeep(items)
+const testItems=[a,b,c];
+const newtestItems=_.flattenDeep(testItems)
 console.log(newItems);
+console.log(newtestItems);
